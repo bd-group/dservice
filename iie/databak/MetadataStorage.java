3e31422457748643d4ad9942f6fbcc373fc1d10b
zhaoyang <xiaofenzy@sina.cn>
2014-01-07 17:24:19 +0800

    add rpc getSFileByid, together with last commit

--- iie/databak/MetadataStorage.java
+++ iie/databak/MetadataStorage.java
@@ -1,17 +1,17 @@
 package iie.databak;
 
 import iie.metastore.MetaStoreClient;
-import iie.databak.RedisFactory;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.apache.hadoop.hive.metastore.IMetaStoreClient;
 import org.apache.hadoop.hive.metastore.api.Database;
 import org.apache.hadoop.hive.metastore.api.GlobalSchema;
 import org.apache.hadoop.hive.metastore.api.Index;
@@ -21,12 +21,10 @@ import org.apache.hadoop.hive.metastore.api.Node;
 import org.apache.hadoop.hive.metastore.api.NodeGroup;
 import org.apache.hadoop.hive.metastore.api.Partition;
 import org.apache.hadoop.hive.metastore.api.PrivilegeBag;
-import org.apache.hadoop.hive.metastore.api.Role;
 import org.apache.hadoop.hive.metastore.api.SFile;
 import org.apache.hadoop.hive.metastore.api.SFileLocation;
 import org.apache.hadoop.hive.metastore.api.Schema;
 import org.apache.hadoop.hive.metastore.api.Table;
-import org.apache.hadoop.hive.metastore.api.User;
 import org.apache.hadoop.hive.metastore.msg.MSGFactory.DDLMsg;
 import org.apache.hadoop.hive.metastore.msg.MSGType;
 import org.apache.thrift.TException;
@@ -56,15 +54,17 @@ public class MetadataStorage {
 	public MetadataStorage(DatabakConf conf) {
 		this.conf = conf;
 		rf = new RedisFactory(conf);
+		
+	}
+
+	public int handleMsg(DDLMsg msg) {
 		try {
-			msClient = new MetaStoreClient(conf.getMshost(), conf.getMsport());
+			if(msClient == null)
+				msClient = new MetaStoreClient(conf.getMshost(), conf.getMsport());
 		} catch (MetaException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-	public int handleMsg(DDLMsg msg) {
 		int eventid = (int) msg.getEvent_id();
 //		System.out.println("handler msg id:"+eventid);
 		try {
@@ -92,6 +92,7 @@ public class MetadataStorage {
 					String dbname = (String) msg.getMsg_data().get("db_name");
 					String tablename = (String) msg.getMsg_data().get("table_name");
 					String old_tablename = (String) msg.getMsg_data().get("old_table_name");
+					Table t = msClient.client.getTable(dbname, tablename);
 					//rm old, put new
 					break;
 				}
@@ -228,12 +229,57 @@ public class MetadataStorage {
 			partitionHm.put(field, (Partition)o);
 	}
 
-	//是不是应该实现成先从缓存中读
-	private Object readObject(String key, String field)throws JedisConnectionException, IOException,ClassNotFoundException {
+	public Object readObject(String key, String field)throws JedisConnectionException, IOException,ClassNotFoundException {
+		Object o = null;
+		if(key.equals(ObjectType.DATABASE))
+			o = databaseHm.get(field);
+		if(key.equals(ObjectType.TABLE))
+			o = tableHm.get(field);
+		if(key.equals(ObjectType.SFILE))
+			o = sFileHm.get(field);
+		if(key.equals(ObjectType.SFILELOCATION))
+			o = sflHm.get(field);
+		if(key.equals(ObjectType.INDEX))
+			o = indexHm.get(field);
+		if(key.equals(ObjectType.NODE))
+			o = nodeHm.get(field);
+		if(key.equals(ObjectType.NODEGROUP))
+			o = nodeGroupHm.get(field);
+		if(key.equals(ObjectType.SCHEMA))
+			o = schemaHm.get(field);
+		if(key.equals(ObjectType.PRIVILEGE))
+			o = privilegeBagHm.get(field);
+		if(key.equals(ObjectType.PARTITION))
+			o = partitionHm.get(field);
+		
+		if(o != null)
+			return o;
+		//SFile 要特殊处理
 		reconnectJedis();
-		ByteArrayInputStream bais = new ByteArrayInputStream(jedis.hget(key.getBytes(), field.getBytes()));
+		if(key.equals(ObjectType.SFILE))
+		{
+			byte[] buf = jedis.hget(key.getBytes(), field.getBytes());
+			if(buf == null)
+				return null;
+			ByteArrayInputStream bais = new ByteArrayInputStream(buf);
+			ObjectInputStream ois = new ObjectInputStream(bais);
+			SFileImage sfi = (SFileImage)ois.readObject();
+			List<SFileLocation> locations = new ArrayList<SFileLocation>();
+			for(int i = 0;i<sfi.getSflkeys().size();i++)
+			{
+				locations.add((SFileLocation)readObject(ObjectType.SFILELOCATION, sfi.getSflkeys().get(i)));
+			}
+			return new SFile(sfi.getFid(),sfi.getDbName(),sfi.getTableName(),sfi.getStore_status(),sfi.getRep_nr(),
+					sfi.getDigest(),sfi.getRecord_nr(),sfi.getAll_record_nr(),locations,sfi.getLength(),
+					sfi.getRef_files(),sfi.getValues(),sfi.getLoad_status());
+		}
+		
+		byte[] buf = jedis.hget(key.getBytes(), field.getBytes());
+		if(buf == null)
+			return null;
+		ByteArrayInputStream bais = new ByteArrayInputStream(buf);
 		ObjectInputStream ois = new ObjectInputStream(bais);
-		Object o = ois.readObject();
+		o = ois.readObject();
 		return o;
 	}
 
