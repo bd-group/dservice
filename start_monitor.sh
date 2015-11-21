4f5823b3b87bf5efb2d67a4250e6ac06322f6846
Ma Can <ml.macana@gmail.com>
2015-11-21 11:53:45 +0800

    Upgrade jetty to 7.6.17

--- start_monitor.sh
+++ start_monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CLASSPATH=lib/servlet-api-2.5.jar:lib/jetty-all-7.0.2.v20100331.jar:build/iie.jar java iie.monitor.MonitorServer 33333 conf/monitor.conf sotstore/sotstore/reports > log/monitor.log & 
+CLASSPATH=lib/servlet-api-2.5.jar:lib/jetty-server-7.6.17.v20150415.jar:lib/jetty-util-7.6.17.v20150415.jar:lib/jetty-http-7.6.17.v20150415.jar:lib/jetty-io-7.6.17.v20150415.jar:lib/jetty-continuation-7.6.17.v20150415.jar:build/iie.jar java iie.monitor.MonitorServer 33333 conf/monitor.conf sotstore/sotstore/reports > log/monitor.log & 
